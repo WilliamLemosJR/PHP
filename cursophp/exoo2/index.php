904450f0b454db2a6de7eb3cadc3190aa5691c08
WilliamLemosJR <127670993+WilliamLemosJR@users.noreply.github.com>
2023-10-17 15:33:41 -0300

    criando a primeira variavel

---
+++ cursophp/exoo2/index.php
@@ -0,0 +1,18 @@
+!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo PHP</title>
+</head>
+<body>
+    <h1>
+        Testando PHP
+    </h1>
+
+    <?php 
+        $nome = "William Lemos";
+        echo " <p> O Meu Nome é $nome </p>" 
+    ?>
+</body>
+</html>
